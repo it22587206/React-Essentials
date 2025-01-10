9e327bff8db4011ae8e58b8aed6f62705a26d022
it22587206 <it22587206@my.sliit.lk>
2025-01-10 21:19:12 +0530

    test 2

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -31,7 +31,8 @@ export default function Examples() {
 
     return (
         <Section title={Examples} id='examples'>
-          <Tabs buttons={
+          <Tabs
+          buttons={
              <>
              <TabButton isSelected={selectedTopic==='components'} 
              onClick={()=>handleSelect('components')}>Components</TabButton>
